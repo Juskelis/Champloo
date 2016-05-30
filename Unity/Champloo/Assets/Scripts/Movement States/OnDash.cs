d17ca0c80dd7e2239862873094ab4b482338832f
Matt Juskelis <matthewjuskelis@gmail.com>
2016-05-30 16:14:49 -0700

    Gravity Modifier for Dash

--- Unity/Champloo/Assets/Scripts/Movement States/OnDash.cs
+++ Unity/Champloo/Assets/Scripts/Movement States/OnDash.cs
@@ -9,6 +9,8 @@ public class OnDash : MovementState
     [SerializeField] private float dashTime;
     private float timeLeft;
 
+    [SerializeField] private float gravityModifier = 1f;
+
     private Vector2 direction;
 
     public override MovementState UpdateState(ref Vector3 velocity)
@@ -16,7 +18,12 @@ public class OnDash : MovementState
         //velocity.x += dashForce * direction.x * dashForce * Time.deltaTime;
         //velocity.y += dashForce * direction.y * dashForce * Time.deltaTime;
 
-        velocity.y -= player.Gravity*Time.deltaTime;
+        velocity.y -= player.Gravity*Time.deltaTime*gravityModifier;
+
+        if(controller.collisions.left || controller.collisions.right)
+        {
+            velocity.x = 0;
+        }
 
         controller.Move(velocity * Time.deltaTime);
 
