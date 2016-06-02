72590402d1b88f186cbfc1212385296abcf0b3ee
Matt Juskelis <matthewjuskelis@gmail.com>
2016-06-01 21:05:14 -0700

    RenderTextures handled correctly

--- Unity/Champloo/Assets/Scripts/SplatRenderer.cs
+++ Unity/Champloo/Assets/Scripts/SplatRenderer.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Rendering;
+using System.Collections.Generic;
 
-public class SplatRenderer : MonoBehaviour {
+public class SplatRenderer : MonoBehaviour
+{
     [SerializeField]
     private FilterMode textureFilterMode = FilterMode.Point;
 
@@ -19,39 +21,53 @@ public class SplatRenderer : MonoBehaviour {
 
     private MeshRenderer child;
 
+    private bool firstPass = true;
+
     void Start()
     {
+
         child = GetComponentInChildren<MeshRenderer>();
 
         Vector3 parentScale = transform.parent.localScale;
 
         //make render texture that fits the object
-        if (parentScale.x < parentScale.y)
-            tex = new RenderTexture(Mathf.CeilToInt(parentScale.x) * scaleFactor/2, Mathf.CeilToInt(parentScale.y) * scaleFactor, 0);
+        if(parentScale.x < parentScale.y)
+            tex = new RenderTexture(Mathf.CeilToInt(parentScale.x) * scaleFactor, Mathf.CeilToInt(parentScale.y) * scaleFactor, 0);
         else
             tex = new RenderTexture(Mathf.CeilToInt(parentScale.x) * scaleFactor, Mathf.CeilToInt(parentScale.y) * scaleFactor/2, 0);
 
         //tex.Create();
-
-        //GL.Clear(true, true, Color.black);
 
         tex.filterMode = FilterMode.Point;
         tex.anisoLevel = 0;
 
         tex.depth = (int)Depth;
 
-        tex.Create();
-
-        tex.DiscardContents();
+        //tex.Create();
 
         //get camera
         cam = GetComponent<Camera>();
-        cam.orthographicSize = Mathf.Min(parentScale.x, parentScale.y) / 2;
+        cam.orthographicSize = parentScale.y/2;
 
         cam.targetTexture = tex;
 
         //cam.clearFlags = CameraClearFlags.Depth;
 
         child.material.mainTexture = tex;
+    }
+
+    void OnPreRender()
+    {
+        if (firstPass)
+        {
+            Color clearColor = new Color(0, 0, 0, 0);
+            GL.Clear(true, true, clearColor);
+            firstPass = false;
+        }
+    }
+
+    void Destroy()
+    {
+        tex.Release();
     }
 }
