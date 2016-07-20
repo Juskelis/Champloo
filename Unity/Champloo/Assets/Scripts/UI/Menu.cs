98a3039ac5d1c25e801f2121ce919ca299f1f0be
Matt Juskelis <matthewjuskelis@gmail.com>
2016-07-20 14:20:18 -0700

    Rearranged Files and Added Menu Transitions

---
+++ Unity/Champloo/Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+    private Animator anim;
+    private CanvasGroup cgroup;
+
+    public bool IsOpen
+    {
+        get { return anim.GetBool("IsOpen"); }
+        set { anim.SetBool("IsOpen", value);}
+    }
+
+    public void Awake()
+    {
+        anim = GetComponent<Animator>();
+        cgroup = GetComponent<CanvasGroup>();
+
+        var rect = GetComponent<RectTransform>();
+        rect.offsetMax = rect.offsetMin = new Vector2(0, 0);
+    }
+
+    public void Update()
+    {
+        cgroup.blocksRaycasts = cgroup.interactable = anim.GetCurrentAnimatorStateInfo(0).IsName("Open");
+    }
+}
