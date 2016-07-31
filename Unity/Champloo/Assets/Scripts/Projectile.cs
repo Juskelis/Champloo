df23fb65b3cfd48026c17f3f2e548471039038f4
Matt Juskelis <matthewjuskelis@gmail.com>
2016-07-31 00:50:41 -0700

    Projectiles don't tunnel

--- Unity/Champloo/Assets/Scripts/Projectile.cs
+++ Unity/Champloo/Assets/Scripts/Projectile.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class Projectile : MonoBehaviour
 {
@@ -12,11 +13,27 @@ public class Projectile : MonoBehaviour
     private bool moving = true;
     public bool Moving { get { return moving; } }
 
+    private Transform follow;
+    private Vector3 relativePos;
+
+    private Rigidbody2D body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
+        body.velocity = Vector2.zero;
         if (moving)
         {
-            transform.Translate(Vector2.right*speed*Time.deltaTime);
+            //transform.Translate(Vector2.right*speed*Time.deltaTime);
+            body.velocity = transform.TransformVector(Vector2.right*speed);
+        }
+        else if(follow != null)
+        {
+            transform.position = follow.position - relativePos;
         }
     }
 
@@ -25,8 +42,29 @@ public class Projectile : MonoBehaviour
         if (!moving) return;
         if ((collisionMask.value & (1 << c.gameObject.layer)) > 0)
         {
-            //transform.parent = c.transform;
+            /*
+            Vector2 average = Vector2.zero;
+            foreach (var contact in c.contacts)
+            {
+                average += contact.point;
+            }
+            transform.position = average/c.contacts.Length;
+            */
+            /*
+            Collider2D col = c.collider;
+            Vector2 pos = transform.position;
+            while (col.OverlapPoint(pos))
+            {
+                pos.x -= transform.right.x;
+                pos.y -= transform.right.y;
+            }
+            transform.position = pos;
+            */
+            follow = c.transform;
+            relativePos = follow.position - transform.position;
             moving = false;
+
+            GetComponent<TrailRenderer>().enabled = false;
         }
     }
 }
