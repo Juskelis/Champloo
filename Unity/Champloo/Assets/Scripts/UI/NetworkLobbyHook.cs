ec2956ba7ec91eb6829f1228a932c2e97bda9d22
Matt Juskelis <matthewjuskelis@gmail.com>
2016-09-23 17:31:58 -0700

    Added NetworkLobbyHook for player settings

---
+++ Unity/Champloo/Assets/Scripts/UI/NetworkLobbyHook.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prototype.NetworkLobby;
+using UnityEngine.Networking;
+
+public class NetworkLobbyHook : LobbyHook {
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+        PlayerSettings playerSettings = gamePlayer.GetComponent<PlayerSettings>();
+
+        playerSettings.Name = lobby.name;
+        playerSettings.Color = lobby.playerColor;
+    }
+}
