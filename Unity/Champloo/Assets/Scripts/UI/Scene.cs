111755da0c57d5134c6418557ccf9448e3188332
Matt Juskelis <matthewjuskelis@gmail.com>
2016-10-21 23:53:49 -0700

    Scene Reloading

--- Unity/Champloo/Assets/Scripts/UI/Scene.cs
+++ Unity/Champloo/Assets/Scripts/UI/Scene.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Prototype.NetworkLobby;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
 
-public class Scene : MonoBehaviour {
+public class Scene : NetworkBehaviour {
+
+    [Command]
+    private void CmdChangeScene(string scene)
+    {
+        NetworkServer.SetAllClientsNotReady();
+        NetworkManager.singleton.ServerChangeScene(scene);
+    }
 
     public void LoadLevel(string s)
     {
-        //SceneManager.LoadScene(s);
-        NetworkManager.singleton.ServerChangeScene(s);
+        SceneManager.LoadScene(s);
+    }
 
+    public void LoadLevelNetworked(string s)
+    {
+        //NetworkManager.singleton.ServerChangeScene(s);
+        //NetworkServer.SetAllClientsNotReady();
+        //LobbyManager.s_Singleton.ServerChangeScene(s);
+        //LobbyManager.s_Singleton.CmdChangeScene(s);
+        CmdChangeScene(s);
+    }
+
+    public void ReturnToLobby()
+    {
+        LobbyManager.s_Singleton.ServerReturnToLobby();
+    }
+
+    public void RestartLevel()
+    {
+        //NetworkManager.singleton.ServerChangeScene(SceneManager.GetActiveScene().name);
+        LoadLevelNetworked(SceneManager.GetActiveScene().name);
     }
 
     public void Quit()
