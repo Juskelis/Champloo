9e1def8b8d11098361760691131d6f1f2b4c5c60
Matt Juskelis <matthewjuskelis@gmail.com>
2016-11-12 12:08:39 -0800

    Change MenuManager to first frame activation
    Once again because of reloading scene nonsense brought on by the
    LobbyManager

--- Unity/Champloo/Assets/Scripts/UI/MenuManager.cs
+++ Unity/Champloo/Assets/Scripts/UI/MenuManager.cs
@@ -1,15 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     public Menu CurrentMenu;
 
-    public void Start()
+    private bool firstFrame = true;
+
+    private void LateUpdate()
+    {
+        if (firstFrame)
+        {
+            firstFrame = false;
+            LoadDefaultMenu();
+        }
+    }
+
+    private void LoadDefaultMenu()
     {
         ShowMenu(CurrentMenu);
     }
-
+    /*
+    public void Start()
+    {
+    }
+    */
     public void ShowMenu(Menu menu)
     {
 
