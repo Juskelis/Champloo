83baf9ea73cd8281b97ba52ad9cf95e0da048a80
Matt Juskelis <matthewjuskelis@gmail.com>
2016-11-18 18:36:06 -0800

    Multiplayer UI Flood Fill
    Will add itself to all connections, so that multiplayer nav is automatic

--- Unity/Champloo/Assets/Scripts/UI/Controller Navigation/MultiplayerSelectable.cs
+++ Unity/Champloo/Assets/Scripts/UI/Controller Navigation/MultiplayerSelectable.cs
@@ -8,12 +8,35 @@ public class MultiplayerSelectable : MonoBehaviour
     [Tooltip("Where player indicators are held (ie Horizontal/Vertical Layout Groups)")]
     public LayoutGroup playerContainerPrefab;
 
+    public bool addToNeighbors = false;
+
     private LayoutGroup playerContainer;
 
     void Start()
     {
         playerContainer = Instantiate(playerContainerPrefab);
         playerContainer.transform.SetParent(transform, false);
+
+        if (addToNeighbors)
+        {
+            Selectable us = GetComponent<Selectable>();
+            if (us.FindSelectableOnDown().GetComponent<MultiplayerSelectable>() == null)
+            {
+                Utility.CopyComponent(this, us.FindSelectableOnDown().gameObject);
+            }
+            if (us.FindSelectableOnUp().GetComponent<MultiplayerSelectable>() == null)
+            {
+                Utility.CopyComponent(this, us.FindSelectableOnUp().gameObject);
+            }
+            if (us.FindSelectableOnLeft().GetComponent<MultiplayerSelectable>() == null)
+            {
+                Utility.CopyComponent(this, us.FindSelectableOnLeft().gameObject);
+            }
+            if (us.FindSelectableOnRight().GetComponent<MultiplayerSelectable>() == null)
+            {
+                Utility.CopyComponent(this, us.FindSelectableOnRight().gameObject);
+            }
+        }
     }
 
     public void AddPlayerIndicator(Transform r)
--- Unity/Champloo/Assets/Scripts/Utility/Utility.cs
+++ Unity/Champloo/Assets/Scripts/Utility/Utility.cs
@@ -5,6 +5,17 @@ using System.Collections;
 
 public class Utility {
 
+    public static T CopyComponent<T>(T original, GameObject destination) where T : Component
+    {
+        System.Type type = original.GetType();
+        Component copy = destination.AddComponent(type);
+        System.Reflection.FieldInfo[] fields = type.GetFields();
+        foreach (System.Reflection.FieldInfo field in fields)
+        {
+            field.SetValue(copy, field.GetValue(original));
+        }
+        return copy as T;
+    }
 
     public static bool IsLayer(LayerMask mask, int layer)
     {
