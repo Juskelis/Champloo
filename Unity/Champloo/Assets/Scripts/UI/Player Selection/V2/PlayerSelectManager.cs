3199adfd69269c143abf1b506e7ea2c2e43ea2c0
Matt Juskelis <matthewjuskelis@gmail.com>
2016-11-25 23:53:05 -0800

    Start Setup of PlayerSelect

---
+++ Unity/Champloo/Assets/Scripts/UI/Player Selection/V2/PlayerSelectManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PlayerSelectManager : MonoBehaviour {
+
+    void Start()
+    {
+    }
+
+    public void PlayerSelected(MultiplayerSelectable selectable, MultiplayerUIController controller)
+    {
+        selectable.GetComponent<PlayerOption>();
+    }
+}
