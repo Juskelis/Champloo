946eb26cce43c720863bace55adcf51df5a4b353
Matt Juskelis <matthewjuskelis@gmail.com>
2016-11-26 16:04:33 -0800

    Updating PlayerLobby on selectable

--- Unity/Champloo/Assets/Scripts/UI/Player Selection/V2/PlayerOption.cs
+++ Unity/Champloo/Assets/Scripts/UI/Player Selection/V2/PlayerOption.cs
@@ -3,15 +3,5 @@ using System.Collections;
 
 public class PlayerOption : MonoBehaviour
 {
-    public Object[] optionData;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public string playerPrefabName;
 }
--- Unity/Champloo/Assets/Scripts/UI/Player Selection/V2/PlayerSelectManager.cs
+++ Unity/Champloo/Assets/Scripts/UI/Player Selection/V2/PlayerSelectManager.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
+using Prototype.NetworkLobby;
 
 public class PlayerSelectManager : MonoBehaviour {
-
-    void Start()
-    {
-    }
-
+    
     public void PlayerSelected(MultiplayerSelectable selectable, MultiplayerUIController controller)
     {
-        selectable.GetComponent<PlayerOption>();
+        PlayerOption selectedOption = selectable.GetComponent<PlayerOption>();
+
+        if(selectedOption != null)
+        {
+            //find the appropriate LobbyPlayer and update information
+            LobbyPlayer player;
+            foreach(var networkPlayer in LobbyManager.s_Singleton.lobbySlots)
+            {
+                player = networkPlayer as LobbyPlayer;
+                if(player != null && player.isLocalPlayer && player.playerControllerId == controller.ControllerNumber)
+                {
+                    print("updating lobby player info");
+                    player.playerName = selectedOption.playerPrefabName;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Selectable does not have a PlayerOption attached to it!");
+        }
     }
 }
