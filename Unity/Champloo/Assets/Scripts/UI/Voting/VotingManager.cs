51c3ce049454a8dd6cfaa6359a8af9ea9f1b53f0
Matt Juskelis <matthewjuskelis@gmail.com>
2016-12-10 13:07:01 -0800

    Made voting manager

---
+++ Unity/Champloo/Assets/Scripts/UI/Voting/VotingManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class VotingManager : MonoBehaviour
+{
+    
+    [Serializable]
+    public class Option
+    {
+        public MultiplayerSelectable selectable;
+        public int rating;
+        [Tooltip("Enable to have the option take priority over others, even if only one person selects it")]
+        public bool forceSelect;
+
+        public int voteCount
+        {
+            get { return selectable.ControllersSelecting(); }
+        }
+    }
+
+    public Option[] options;
+    private Option electedOption;
+
+    private Option TallyVotes()
+    {
+        Option mostVotes = null;
+        Option ret = null;
+        foreach (Option option in options)
+        {
+            if (!option.forceSelect)
+            {
+                //normal tallying
+                if (mostVotes == null && option.voteCount > 0)
+                {
+                    mostVotes = option;
+                } else if (mostVotes != null && mostVotes.voteCount*mostVotes.rating < option.voteCount*option.rating)
+                {
+                    mostVotes = option;
+                }
+            }
+            else
+            {
+                //force tallying
+                if (ret == null && option.selectable.ControllersSelecting() > 0)
+                {
+                    ret = option;
+                } else if (ret != null && ret.voteCount < option.voteCount*option.rating)
+                {
+                    ret = option;
+                }
+            }
+        }
+
+        return ret ?? mostVotes;
+    }
+
+    public void OnVote(MultiplayerSelectable selected, MultiplayerUIController controller)
+    {
+        electedOption = TallyVotes();
+    }
+}
