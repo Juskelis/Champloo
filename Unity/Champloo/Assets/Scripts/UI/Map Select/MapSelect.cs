f0bb22cb653b87a18a09b610b1e5629f95723d22
Matt Juskelis <matthewjuskelis@gmail.com>
2017-01-14 16:38:17 -0800

    Map Select

---
+++ Unity/Champloo/Assets/Scripts/UI/Map Select/MapSelect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prototype.NetworkLobby;
+
+public class MapSelect : MonoBehaviour {
+    public void SetMap(MultiplayerSelectable selectable, MultiplayerUIController controller)
+    {
+        MapOption option = selectable.GetComponent<MapOption>();
+        if(option != null)
+        {
+            LobbyManager.s_Singleton.playScene = option.mapName;
+        }
+        else
+        {
+            Debug.LogError("Selectable does not have a MapOption");
+        }
+    }
+}
