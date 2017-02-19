3e1a59f72d9e92e21116e56eb1feb441c21e220a
Matt Juskelis <matthewjuskelis@gmail.com>
2017-02-19 13:43:22 -0800

    Movement State Foot Dust

---
+++ Unity/Champloo/Assets/Scripts/Effects/Foot Dust/MovementStateFootDust.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TypeReferences;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class MovementStateFootDust : MonoBehaviour
+{
+    [ClassExtends(typeof(MovementState))]
+    public ClassTypeReference movementState;
+
+    private ParticleSystem particle;
+    private Player p;
+
+    private bool playing = true;
+
+    void Awake()
+    {
+        particle = GetComponent<ParticleSystem>();
+        p = GetComponentInParent<Player>();
+        if (p == null)
+        {
+            throw new ArgumentNullException();
+        }
+    }
+
+    void Update()
+    {
+        if (p.CurrentMovementState.GetType() == movementState.Type)
+        {
+            if(!playing) SetPlaying(true);
+        }
+        else
+        {
+            if(playing) SetPlaying(false);
+        }
+    }
+
+    void SetPlaying(bool playing)
+    {
+        this.playing = playing;
+        ParticleSystem.EmissionModule module = particle.emission;
+        module.enabled = playing;
+    }
+}
