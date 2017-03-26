09916d1c0f1510bb2ad4ca66d89ca27d3fb97aac
Matt Juskelis <matthewjuskelis@gmail.com>
2017-03-25 18:47:44 -0700

    Tiling Textures added to level geometry

---
+++ Unity/Champloo/Assets/Scripts/Utility/TextureScaler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class TextureScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    float scaleDim = Mathf.Min(transform.localScale.x, transform.localScale.y);
+        GetComponent<Renderer>().material.mainTextureScale = new Vector2(
+            transform.localScale.x/scaleDim,
+            transform.localScale.y/scaleDim
+        );
+	}
+}
