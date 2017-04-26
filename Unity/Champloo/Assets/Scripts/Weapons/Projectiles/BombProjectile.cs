cdc2a5f3f079d84de8eb9a572370221b676f8f64
Matt Juskelis <matthewjuskelis@gmail.com>
2017-04-25 23:10:45 -0700

    Add Bomb projectile

---
+++ Unity/Champloo/Assets/Scripts/Weapons/Projectiles/BombProjectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BombProjectile : Projectile {
+    [SerializeField]
+    private float fuseLength;
+
+    private float fuseTimeLeft = 0f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        fuseTimeLeft -= Time.deltaTime;
+    }
+
+    protected override void ProcessHitPlayer(GameObject o)
+    {
+        //turn off parent functionality
+    }
+
+    protected override void ProcessHitObstacle(Collider2D c)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            transform.forward,
+            Mathf.Infinity,
+            obstacleMask);
+        Vector3 bounce = -(Vector3.Reflect(transform.forward, hit.normal).normalized);
+        float angleToBounce = Vector3.Angle(transform.forward, bounce);
+        transform.Rotate(0, 0, angleToBounce);
+    }
+}
