411cc01b7d4e9af78c59e9ef5b305f21e6a468e6
Matt Juskelis <matthewjuskelis@gmail.com>
2017-04-25 23:10:33 -0700

    Change interface for Projectile again

--- Unity/Champloo/Assets/Scripts/Weapons/Projectiles/Projectile.cs
+++ Unity/Champloo/Assets/Scripts/Weapons/Projectiles/Projectile.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using System.Linq;
 using TypeReferences;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Projectile : MonoBehaviour
 {
     public int PlayerNumber { get; set; }
 
-    [SerializeField] private LayerMask obstacleMask;
+    [SerializeField] protected LayerMask obstacleMask;
 
     [SerializeField] private float speed = 1f;
 
@@ -19,12 +20,12 @@ public class Projectile : MonoBehaviour
 
     private Rigidbody2D body;
 
-    void Awake()
+    protected virtual void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    protected virtual void Update()
     {
         body.velocity = Vector2.zero;
         if (moving)
@@ -38,28 +39,33 @@ public class Projectile : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D c)
+    protected virtual void OnTriggerEnter2D(Collider2D c)
     {
-        ProcessHit(c.gameObject);
+        ProcessHitPlayer(c.gameObject);
 
         //check whether we need to stop
         if (moving && (obstacleMask.value & (1 << c.gameObject.layer)) > 0)
         {
-            follow = c.transform;
-            relativePos = follow.position - transform.position;
-            moving = false;
-
-            EZCameraShake.CameraShaker.Instance.ShakeOnce(5, 5, 0, 0.5f);
-
-            GetComponent<TrailRenderer>().enabled = false;
+            ProcessHitObstacle(c);
         }
     }
 
-    protected void ProcessHit(GameObject g)
+    protected virtual void ProcessHitPlayer(GameObject g)
     {
-        Player p = g.GetComponent<Player>();
-        p = p ?? g.GetComponentInParent<Player>();
+        Player p = g.gameObject.GetComponent<Player>()
+                   ?? g.gameObject.GetComponentInParent<Player>();
         if (p == null) return;
         p.GetHit(this);
+    }
+    
+    protected virtual void ProcessHitObstacle(Collider2D c)
+    {
+        follow = c.transform;
+        relativePos = follow.position - transform.position;
+        moving = false;
+
+        EZCameraShake.CameraShaker.Instance.ShakeOnce(5, 5, 0, 0.5f);
+
+        GetComponent<TrailRenderer>().enabled = false;
     }
 }
