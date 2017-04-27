83f50021cdf1ecdadc5b02cfa345f0aea375f9cd
Matt Juskelis <matthewjuskelis@gmail.com>
2017-04-26 21:33:44 -0700

    Add OnBomb MovementSpecial

---
+++ Unity/Champloo/Assets/Scripts/Player/Movement States/Movement Specials/OnBomb.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnBomb : OnMovementSpecial
+{
+    [SerializeField]
+    private BombProjectile bombProjectile;
+
+    private BombProjectile droppedBomb;
+
+    private bool isThrown;
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+        droppedBomb = Instantiate(
+            bombProjectile,
+            transform.position,
+            Quaternion.AngleAxis(
+                Utility.Vector2AsAngle(Vector2.down),
+                transform.forward
+            )
+        );
+    }
+
+    public override MovementState DecideNextState(Vector3 velocity, Vector3 externalForces)
+    {
+        if (timingState == TimingState.DONE)
+        {
+            return GetSimulatedState();
+        }
+        return null;
+    }
+}
