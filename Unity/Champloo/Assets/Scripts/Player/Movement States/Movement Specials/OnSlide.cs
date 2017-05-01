4af2b8466b030bee2797a8d71b792e02660bc5e0
JXVD <jackvangame@live.com>
2017-05-01 15:37:57 -0700

    Bugfixes
    fixed state change system, slide direction

--- Unity/Champloo/Assets/Scripts/Player/Movement States/Movement Specials/OnSlide.cs
+++ Unity/Champloo/Assets/Scripts/Player/Movement States/Movement Specials/OnSlide.cs
@@ -14,10 +14,14 @@ public class OnSlide : OnMovementSpecial
     private Vector2 groundSlideForce;
     [SerializeField]
     private float groundSlideDecay;
-    
+
+    [SerializeField]
+    private PlayRandomSource wallSlideSound;
+
     private bool goingRight = true;
 
     private bool goingUp = true;
+
 
     public override Vector3 ApplyFriction(Vector3 velocity)
     {
@@ -25,14 +29,51 @@ public class OnSlide : OnMovementSpecial
         if (timingState != TimingState.IN_PROGRESS || GetSimulatedState() == GetComponent<InAir>())
         {
             return GetSimulatedState().ApplyFriction(velocity);
-        } 
-        if(GetSimulatedState() == GetComponent<OnWall>())
+        }
+
+        goingRight = velocity.x > 0;
+        if (velocity.x == 0) goingRight = player.AimDirection.x > 0;
+        if (GetSimulatedState() == GetComponent<OnWall>())
         {
-            velocity.y += (goingUp ? wallSlideUpDecay: -wallSlideDownDecay);
+            //if sliding down and hit ground;
+            //this may not work as GetSimulatedState may return OnGround before this, so we'll need to change if that is the case
+            if (!goingUp && controller.collisions.Below)
+            {
+                Debug.Log("on wall, detected collison below");
+                if (controller.collisions.Left)
+                {
+                    goingRight = true;
+                    velocity.x += velocity.y;
+                }
+                else if(controller.collisions.Right)
+                {
+                    goingRight = false;
+                    velocity.x -= velocity.y;
+                }
+            }
+            else
+            {
+                velocity.y += (goingUp ? wallSlideUpDecay : -wallSlideDownDecay);
+            }
         }
         else if (GetSimulatedState() == GetComponent<OnGround>())
         {
-            velocity.x += (goingRight ? groundSlideDecay : -groundSlideDecay);
+            //if the player hits something 
+            if (controller.collisions.Right && goingRight)
+            {
+                velocity.y += velocity.x;
+            }
+            else if (controller.collisions.Left && !goingRight)
+            {
+                velocity.y += -velocity.x;
+            }
+            else
+            {
+                //add in any potential -y velocity if there is any, otherwise just decay the speed
+
+                velocity.x += (goingRight ? (groundSlideDecay - velocity.y) : -(groundSlideDecay - velocity.y));
+                velocity.y = 0;
+            }
         }
         return velocity;
     }
@@ -43,6 +84,13 @@ public class OnSlide : OnMovementSpecial
         {
             return GetSimulatedState();
         }
+        //if InAir, or hit something from below
+        else if(GetSimulatedState() == GetComponent<InAir>() || (GetSimulatedState() == GetComponent<OnWall>() && controller.collisions.Above))
+        {
+            return GetComponent<InAir>();
+        }
+
+
         return null;
     }
 
@@ -50,25 +98,17 @@ public class OnSlide : OnMovementSpecial
     {
         base.OnEnter(inVelocity, inExternalForces, out outVelocity, out outExternalForces);
         goingRight = inVelocity.x > 0;
+        //if a player isn't moving, get their aim direction/direction they are facing, otherwise you can only slide the way you are running
+        if (inVelocity.x == 0) goingRight = player.AimDirection.x > 0; 
         goingUp = inVelocity.y > 0;
         //if onWall
         if (GetSimulatedState() == GetComponent<OnWall>())
         {
-            outVelocity.y = goingUp ? wallSlideForce.x : -wallSlideForce.x;
+            outVelocity.y = goingUp ? wallSlideForce.y : -wallSlideForce.y;
         }
         else if (GetSimulatedState() == GetComponent<OnGround>())
         {
             outVelocity.x = goingRight ? groundSlideForce.x : -groundSlideForce.x;
         }
-    }
-
-    protected override void OnStart()
-    {
-        base.OnStart();
-    }
-
-    protected override void OnEnd()
-    {
-        base.OnEnd();
     }
 }
