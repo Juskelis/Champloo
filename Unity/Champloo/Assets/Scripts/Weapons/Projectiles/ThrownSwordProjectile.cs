68d261c0a933fabd748c16ddbfed5e90aaa6d08a
Matt Juskelis <matthewjuskelis@gmail.com>
2017-05-25 18:46:27 -0700

    Create new ThrownSwordProjectile

---
+++ Unity/Champloo/Assets/Scripts/Weapons/Projectiles/ThrownSwordProjectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrownSwordProjectile : Projectile {
+    protected override void ProcessHitPlayer(GameObject g)
+    {
+        Player p = g.GetComponent<Player>()??g.GetComponentInParent<Player>();
+        if (player == null || player.CurrentMovementState is InBlock) return;
+        base.ProcessHitPlayer(g);
+    }
+}
