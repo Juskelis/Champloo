5e735aff5a3ced7509e9e07b47f6520988671a73
Matt Juskelis <matthewjuskelis@gmail.com>
2017-05-25 19:12:34 -0700

    Fix bugs

--- Unity/Champloo/Assets/Scripts/Weapons/Projectiles/ThrownSwordProjectile.cs
+++ Unity/Champloo/Assets/Scripts/Weapons/Projectiles/ThrownSwordProjectile.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class ThrownSwordProjectile : Projectile {
     protected override void ProcessHitPlayer(GameObject g)
     {
-        Player p = g.GetComponent<Player>()??g.GetComponentInParent<Player>();
-        if (player == null || player.CurrentMovementState is InBlock) return;
-        base.ProcessHitPlayer(g);
+        Player p = g.GetComponent<Player>()
+                   ?? g.GetComponentInParent<Player>();
+        if (p == null) return;
+        if (p.CurrentMovementState is InBlock)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            base.ProcessHitPlayer(g);
+        }
     }
 }
