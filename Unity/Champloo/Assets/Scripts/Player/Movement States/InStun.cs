7cb61d5b3ba143aca7c667be460c4bbae8d434ec
Matt Juskelis <matthewjuskelis@gmail.com>
2017-06-01 18:17:58 -0700

    Create stun state

---
+++ Unity/Champloo/Assets/Scripts/Player/Movement States/InStun.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InStun : MovementState
+{
+    [SerializeField]
+    private float defaultStunDuration;
+
+    private float stunDuration;
+    private float stunTimer;
+    private bool setStunDuration = false;
+
+    private MovementState getSimulatedState()
+    {
+        if (controller.collisions.Below)
+        {
+            return GetComponent<OnGround>();
+        }
+        else if (controller.collisions.Left || controller.collisions.Right)
+        {
+            return GetComponent<OnWall>();
+        }
+        return GetComponent<InAir>();
+    }
+
+    public void SetStunTime(float time)
+    {
+        stunDuration = time;
+        setStunDuration = true;
+    }
+
+    public override Vector3 ApplyFriction(Vector3 velocity)
+    {
+        return getSimulatedState().ApplyFriction(velocity);
+    }
+
+    public override MovementState DecideNextState(Vector3 velocity, Vector3 externalForces)
+    {
+        stunTimer -= Time.deltaTime;
+        if (stunTimer <= 0)
+        {
+            return getSimulatedState();
+        }
+        return null;
+    }
+
+    public override void OnEnter(Vector3 inVelocity, Vector3 inExternalForces, out Vector3 outVelocity, out Vector3 outExternalForces)
+    {
+        base.OnEnter(inVelocity, inExternalForces, out outVelocity, out outExternalForces);
+        stunTimer = setStunDuration ? stunDuration : defaultStunDuration;
+        print("stunned");
+    }
+}
