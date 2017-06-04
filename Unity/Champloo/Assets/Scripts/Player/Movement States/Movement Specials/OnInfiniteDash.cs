2a1005407ccab0275c5423e7ef8d32257ccbf916
JXVD <jackvangame@live.com>
2017-06-03 19:47:30 -0700

    Added Infinite Dash
    -Created OnInfiniteDash as child of OnDash
    -Changed OnDash to accomodate for inheritance
    -Removed left in Debug Statement in Particle System Destroyer

---
+++ Unity/Champloo/Assets/Scripts/Player/Movement States/Movement Specials/OnInfiniteDash.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnInfiniteDash : OnDash
+{
+    public override MovementState DecideNextState(Vector3 velocity, Vector3 externalForces)
+    {
+        specialTimeLeft -= Time.deltaTime;
+
+        //Buffer for attacks and movement specials done too early
+        if (specialTimeLeft < (specialTime * nextDashBufferWindow))
+        {
+            if (player.InputPlayer.GetButtonDown("Attack"))
+            {
+                earlyAttackInput = true;
+            }
+            else if (player.InputPlayer.GetButtonDown("Movement Special"))
+            {
+                hitNextDashInput = true;
+                currentDashes++;
+            }
+        }
+
+        if (isDisabled || timingState == TimingState.DONE)
+        {
+            if (earlyAttackInput)
+            {
+                return GetComponent<InAttack>();
+            }
+            else if (hitNextDashInput)
+            {
+                hitNextDashInput = false;
+                return GetComponent<OnInfiniteDash>();
+            }
+            else if (controller.collisions.Below)
+            {
+                return GetComponent<OnGround>();
+            }
+            else if (controller.collisions.Left || controller.collisions.Right)
+            {
+                return GetComponent<OnWall>();
+            }
+            return GetComponent<InAir>();
+        }
+
+        return null;
+    }
+
+    //remove functionality of OnDash's OnEnterWall
+    public override void OnEnterWall(Vector3 velocity, Vector3 externalForces)
+    {
+    }
+}
