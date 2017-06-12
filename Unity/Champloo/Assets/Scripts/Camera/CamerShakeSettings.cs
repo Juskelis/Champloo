b2bc98b3fb3f5362d316c1dd4162988b5f280cb7
Matt Juskelis <matthewjuskelis@gmail.com>
2017-06-11 22:00:52 -0700

    Create Camera Shake Settings struct

---
+++ Unity/Champloo/Assets/Scripts/Camera/CamerShakeSettings.cs
@@ -0,0 +1,9 @@
+﻿[System.Serializable]
+public struct CamerShakeSettings
+{
+    public bool shake;
+    public float magnitude;
+    public float roughness;
+    public float fadeInTime;
+    public float fadeOutTime;
+}
