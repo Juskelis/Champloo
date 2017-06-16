e17c2b2b4599002908e45a036090fd18cd91c55d
Matt Juskelis <matthewjuskelis@gmail.com>
2017-06-15 22:52:35 -0700

    Adding Aiming Indicators

---
+++ Unity/Champloo/Assets/Scripts/UI/Player/AimingIndicator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimingIndicator : MonoBehaviour
+{
+    private Player p;
+
+    void Awake()
+    {
+        p = GetComponentInParent<Player>();
+    }
+
+    void Update()
+    {
+        Vector2 aim = p.AimDirection;
+        transform.rotation = Quaternion.AngleAxis(
+            Utility.Vector2AsAngle(aim),
+            transform.forward
+        );
+    }
+}
