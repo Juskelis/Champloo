ff42bb12f60cd88025ccb869176a946779b9060e
Matt Juskelis <matthewjuskelis@gmail.com>
2017-06-25 15:34:13 -0700

    Add more colors

--- Unity/Champloo/Assets/Lobby/Scripts/Lobby/LobbyPlayer.cs
+++ Unity/Champloo/Assets/Lobby/Scripts/Lobby/LobbyPlayer.cs
@@ -12,7 +12,17 @@ namespace Prototype.NetworkLobby
     //Any LobbyHook can then grab it and pass those value to the game player prefab (see the Pong Example in the Samples Scenes)
     public class LobbyPlayer : NetworkLobbyPlayer
     {
-        static Color[] Colors = new Color[] { Color.magenta, Color.red, Color.cyan, Color.blue, Color.green, Color.yellow };
+        static Color[] Colors = new Color[]
+        {
+            Color.magenta,
+            Color.red,
+            Color.cyan,
+            Color.blue,
+            Color.green,
+            Color.yellow,
+            Color.black,
+            Color.gray
+        };
         //used on server to avoid assigning the same color to two player
         static List<int> _colorInUse = new List<int>();
 
