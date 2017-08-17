2cc76070b2f4df9d68de03c2baa35323e0dd61d2
Matt Juskelis <matthewjuskelis@gmail.com>
2017-08-16 23:07:02 -0700

    Projectile effects

---
+++ Unity/Champloo/Assets/Scripts/Events/Event Listeners/ProjectileHitParticles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class ProjectileHitParticles : MonoBehaviour {
+
+    [SerializeField]
+    GameObject effect;
+
+    void Start()
+    {
+        EventDispatcher.Instance.AddListener<ProjectileHitEvent>(OnProjectileHit);
+    }
+
+    void OnProjectileHit(object sender, EventArgs args)
+    {
+        Projectile proj = (Projectile)sender;
+        ProjectileHitEvent projectileHitEvent = (ProjectileHitEvent) args;
+        if (projectileHitEvent.HitPlayer == null
+            || proj.OurPlayer.PlayerNumber != projectileHitEvent.HitPlayer.PlayerNumber)
+        {
+            Instantiate(effect, proj.transform.position, proj.transform.rotation);
+        }
+    }
+}
