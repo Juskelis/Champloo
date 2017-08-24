e5511bbbee8b4034604f8f14ed014cee72723f16
Matt Juskelis <matthewjuskelis@gmail.com>
2017-08-23 17:40:48 -0700

    Create kill line

---
+++ Unity/Champloo/Assets/Scripts/Events/Event Listeners/WeaponKillListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponKillListener : MonoBehaviour
+{
+    [SerializeField]
+    private Transform effect;
+
+    void Start()
+    {
+        GetComponentInParent<LocalEventDispatcher>().AddListener<KillEvent>(OnKill);
+    }
+
+    private void OnKill(object sender, EventArgs args)
+    {
+        KillEvent e = (KillEvent)args;
+
+        Vector3 dirToKiller = (e.Killer.transform.position - e.Victim.transform.position).normalized;
+        Instantiate(
+            effect,
+            e.Victim.transform.position,
+            Quaternion.AngleAxis(
+                    Utility.Vector2AsAngle(dirToKiller),
+                    transform.forward));
+    }
+}
