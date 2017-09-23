680a154a212a0f16bb8c07b0ffb8823327cf692b
Matt Juskelis <matthewjuskelis@gmail.com>
2017-09-23 14:13:21 -0700

    Make Stun have consistent friction
    It was listening to inputs before, but now it isn't

--- Unity/Champloo/Assets/Scripts/Player/Movement States/InStun.cs
+++ Unity/Champloo/Assets/Scripts/Player/Movement States/InStun.cs
@@ -4,31 +4,22 @@ using UnityEngine;
 
 public class InStun : MovementState
 {
-    public override bool AttackAllowed { get { return false; } }
+    [SerializeField]
+    private float friction;
 
-    private MovementState getSimulatedState()
-    {
-        if (controller.collisions.Below)
-        {
-            return GetComponent<OnGround>();
-        }
-        else if (controller.collisions.Left || controller.collisions.Right)
-        {
-            return GetComponent<OnWall>();
-        }
-        return GetComponent<InAir>();
-    }
+    public override bool AttackAllowed { get { return false; } }
 
     public override Vector3 ApplyFriction(Vector3 velocity)
     {
-        return getSimulatedState().ApplyFriction(velocity);
+        velocity.x = Mathf.MoveTowards(velocity.x, 0, friction * Time.deltaTime);
+        return velocity;
     }
 
     public override MovementState DecideNextState(Vector3 velocity, Vector3 externalForces)
     {
         if (!player.Stunned)
         {
-            return getSimulatedState();
+            return GetSimulatedState();
         }
         return null;
     }
