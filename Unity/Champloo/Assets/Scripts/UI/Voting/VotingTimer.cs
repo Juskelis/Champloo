a3b44e029f58eed71152df57a717603d42b32ac7
Matt Juskelis <matthewjuskelis@gmail.com>
2017-09-26 20:41:50 -0700

    Add a Voting Timer UI script

---
+++ Unity/Champloo/Assets/Scripts/UI/Voting/VotingTimer.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VotingTimer : MonoBehaviour
+{
+    public Text timer;
+
+    public VotingManager manager;
+
+    void Update()
+    {
+        timer.enabled = manager.hasVotingTimeout && manager.VotingTimeLeft > 0;
+        timer.text = Mathf.CeilToInt(manager.VotingTimeLeft).ToString(CultureInfo.InvariantCulture);
+    }
+}
