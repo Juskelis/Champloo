a6a8c8ba85ae2f04acbe0a9d5652d71f726e6145
Matt Juskelis <matthewjuskelis@gmail.com>
2017-10-05 21:12:52 -0700

    Create Special listener

---
+++ Unity/Champloo/Assets/Scripts/Events/Event Listeners/WeaponSpecialListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class WeaponSpecialListener : MonoBehaviour {
+    
+    [SerializeField]
+    private Transform buildupEffect;
+
+    void Awake()
+    {
+        GetComponentInParent<LocalEventDispatcher>().AddListener<WeaponSpecialTimingEvent>(OnSpecial);
+    }
+
+    private void OnSpecial(object sender, EventArgs args)
+    {
+        WeaponSpecialTimingEvent e = (WeaponSpecialTimingEvent)args;
+        if (e.Timing == TimingState.WARMUP)
+        {
+            Instantiate(buildupEffect, transform, false);
+        }
+    }
+}
