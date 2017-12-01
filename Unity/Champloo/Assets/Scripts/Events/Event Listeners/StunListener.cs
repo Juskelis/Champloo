5ab3d0f17125fb325f9b7588543d51a4b281e5a5
Matt Juskelis <matthewjuskelis@gmail.com>
2017-11-30 20:17:46 -0800

    Create a stun listener to spawn effects
    It also turns off the effect at the end

---
+++ Unity/Champloo/Assets/Scripts/Events/Event Listeners/StunListener.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class StunListener : MonoBehaviour
+{
+    [SerializeField]
+    private Transform toSpawnOnStart;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    [SerializeField]
+    private Transform toSpawnOnEnd;
+
+    private Transform spawnedStunEffect;
+
+    void Start()
+    {
+        GetComponent<LocalEventDispatcher>().AddListener<MovementStateChangedEvent>(OnTiming);
+    }
+
+    private void OnTiming(object sender, EventArgs args)
+    {
+        MovementStateChangedEvent e = (MovementStateChangedEvent)args;
+        if (e.Next is InStun)
+        {
+            if (toSpawnOnStart != null)
+            {
+                spawnedStunEffect = Instantiate(toSpawnOnStart);
+                spawnedStunEffect.SetParent(transform, false);
+                spawnedStunEffect.Translate(offset);
+            }
+        }
+        else
+        {
+            if (spawnedStunEffect != null)
+            {
+                spawnedStunEffect.GetComponentInChildren<ParticleSystem>().Stop();
+                spawnedStunEffect = null;
+            }
+
+            if (toSpawnOnEnd != null)
+            {
+                Transform spawned = Instantiate(toSpawnOnEnd);
+                spawned.SetParent(transform, false);
+            }
+        }
+    }
+}
