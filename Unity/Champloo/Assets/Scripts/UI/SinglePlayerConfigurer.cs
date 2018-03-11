5978c1109ecb88d8fb54af1cc005d806a67ed1a9
Matt Juskelis <matthewjuskelis@gmail.com>
2018-03-10 16:00:10 -0800

    Single Player Recongifuring on Return
    Entering level in practice and then returning causes the mintoadvance to
    be 2. This fixes that.

--- Unity/Champloo/Assets/Scripts/UI/SinglePlayerConfigurer.cs
+++ Unity/Champloo/Assets/Scripts/UI/SinglePlayerConfigurer.cs
@@ -1,26 +1,37 @@
 ﻿using Prototype.NetworkLobby;
 using UnityEngine;
 
+/// <summary>
+/// You may be wondering "why are the default values static? why are only setting them if theyre less than zero???"
+/// It turns out that Awake is called every time the script is LOADED, which happens every time the scene changes.
+/// The script actually resets itself entirely for all instance variables. This is *bad* for something that is
+/// persisting across scenes, since itll get mixed up in between scenes.
+/// </summary>
 public class SinglePlayerConfigurer : MonoBehaviour
 {
-
-    private int defaultMinPlayers;
-    private int defaultMaxPlayers;
+    private static int defaultMinPlayers = -1;
+    private static int defaultMaxPlayers = -1;
 
     private LobbyManager manager;
 
     private MultiplayerUIManager[] uiManagers;
 
-    void Start()
+    void Awake()
     {
         manager = LobbyManager.s_Singleton ?? FindObjectOfType<LobbyManager>();
         uiManagers = FindObjectsOfType<MultiplayerUIManager>();
-        foreach(MultiplayerUIManager manager in uiManagers)
+
+        //awake gets called multiple times turns out,
+        // so we need a way to keep track of ACTUAL awake
+        if (defaultMaxPlayers < 0 || defaultMinPlayers < 0)
         {
-            manager.MinPlayersToAdvance = 2;
+            defaultMinPlayers = manager.minPlayers;
+            defaultMaxPlayers = manager.maxPlayers;
+            foreach (MultiplayerUIManager manager in uiManagers)
+            {
+                manager.MinPlayersToAdvance = defaultMinPlayers;
+            }
         }
-        defaultMinPlayers = manager.minPlayers;
-        defaultMaxPlayers = manager.maxPlayers;
     }
 
     public void SetSinglePlayer(bool singlePlayer)
